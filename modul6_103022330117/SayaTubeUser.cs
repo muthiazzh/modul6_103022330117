ef82b52b0ae1f39add12576acc47aa9eee4dd5cc
muthiazzh <muthiahazzahra200@gmail.com>
2025-03-25 08:12:38 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_103022330117/SayaTubeUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022330117
+{
+    class SayaTubeUser 
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        string Username;
+
+        public SayaTubeUser(string username)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.uploadedVideos = new List<SayaTubeVideo>();
+            Username = username;
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int totalPlay = 0;
+            
+            foreach (var Video in uploadedVideos){
+                totalPlay += video.getPlayCount();
+            }
+            return GetTotalVideoPlayCount();
+        }
+
+        public void AddVideo(List<SayaTubeUser> uploadedVideos)
+        {
+            
+            List<SayaTubeUser> uploadedVideo = new List<SayaTubeUser>();
+            for ()
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("User: " + Username);
+            Console.WriteLine("Video 1 judul: " + uploadedVideos);
+            Console.WriteLine("Video 2 judul: " + uploadedVideos);
+        }
+    }
+}
---
+++ modul6_103022330117/SayaTubeVideo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022330117
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentException("Title cannot be null or empty");
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public int getPlayCount()
+        {
+            return playCount;
+        }
+
+        public void IncrasePlayCount(int count)
+        {
+            if (count < 0)
+                throw new ArgumentException("play count cannot be negative");
+
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+}
